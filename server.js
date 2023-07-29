c05d598ff423dab3e3e43b59492b4e2948deb8dc
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-07-29 14:36:55 -0700

    setup the mysql connection on the server.js file using the dotenv module.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
+require('dotenv').config();
+
 const app = express();
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log('Connected to database? SUCCESS')
+)
 
 app.use(express.json());
 
