17a0d31a07dcc047c93c7793fb34c568da912251
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-07-29 12:14:21 -0700

    created initial file structure and added seed data for the employees table that will be used.

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+insert into employee (id, first_name, last_name, role_id, manager_id) 
+values 
+		(1, 'Bord', 'Crowch', 1, 1),
+		(2, 'Ronnica', 'Sackes', 2, 2),
+		(3, 'Tiphany', 'Lyenyng', 3, 3),
+		(4, 'Maribelle', 'Witnall', 4, 4),
+		(5, 'Roth', 'Dutt', 5, 5),
+		(6, 'Elroy', 'Stitle', 6, 6),
+		(7, 'Irvine', 'Louisot', 7, 7),
+		(8, 'Erinn', 'Preene', 8, 8),
+		(9, 'Genovera', 'Wandrey', 9, 9),
+		(10, 'Ximenez', 'Delaney', 10, 10),
+		(11, 'Kristi', 'Tabord', 11, 11),
+		(12, 'Shelia', 'Taffurelli', 12, 12),
+		(13, 'Chery', 'Trolley', 13, 13),
+		(14, 'Arlen', 'Starmore', 14, 14),
+		(15, 'Ethelbert', 'Philippou', 15, 15);
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const QueriesSQL = class{
+    constructor(){}
+}
+
+modules.export = QueriesSQL;
